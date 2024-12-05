695d5795a94a854352297c63d1226341bb13721f
Sourabh <153990793+sourabhjoshi62@users.noreply.github.com>
2024-12-05 10:26:28 +0000

    conversion in js

---
+++ 01_JSbasic/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score ="33abc"
+let valueNum = Number(score)
+console.log(typeof valueNum)
+console.log(valueNum)
+
+
+let age = null
+let value = Number(age)
+console.log(typeof value)
+console.log(value)
+
+
+/*
+"33"=33
+"33abc"=Nan
+true=1
+undefined=Nan
+null=0 
+*/
+
+
+let boolval=0
+let convert=Boolean(boolval)
+console.log(typeof convert)
+console.log(convert)
+
