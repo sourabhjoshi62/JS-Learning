a6965083154436a601dffa0609dd9976d71f8ec9
Sourabh <153990793+sourabhjoshi62@users.noreply.github.com>
2024-12-05 10:04:19 +0000

    basics of variables and datatype

---
+++ 01_JSbasic/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword ="12345"
+accountCity = "Jaipur"
+let accountstate
+
+//accountId = 2
+/*prefer not to use var because of issues in block scope and functional scope*/
+
+//console.log(accountId);
+console.log([accountId,accountEmail,accountPassword,accountCity,accountstate]);
---
+++ 01_JSbasic/02_datatype.js
@@ -0,0 +1,8 @@
+console.log(typeof "Sourabh")
+
+let age=21
+console.log(typeof age)
+
+console.log(typeof null)
+
+console.log(typeof undefined)
